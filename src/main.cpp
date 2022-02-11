bd0249b1dfb18fe1780404df29be590204a572ea
Suhanov <ivan.suhanov5@yandex.ru>
2022-02-11 18:56:28 +0300

    Refactoring

--- src/main.cpp
+++ src/main.cpp
@@ -8,20 +8,21 @@
 
 CRGB leds[LED_COUNT];
 byte counter;
+uint32_t myTimer;
+double bright = 1;
 
 void setup() {
   Serial.begin(9600);
   pinMode(PIN_MOVE_SENSOR, INPUT);
   pinMode(LED_PIN, OUTPUT);
   FastLED.addLeds< WS2812, LED_PIN, GRB>(leds, LED_COUNT);
-  FastLED.setBrightness(100);
+  FastLED.setBrightness(100 * bright);
   FastLED.showColor(HUE_RED);
   // put your setup code here, to run once:
 }
 
 void loop() {
 int pirVal = digitalRead(PIN_MOVE_SENSOR);
-Serial.println(digitalRead(PIN_MOVE_SENSOR));
  
   for (int i = 0; i < LED_COUNT; i++) {
     leds[i].setHue(counter + i * 255 / LED_COUNT);
@@ -31,22 +32,30 @@ Serial.println(digitalRead(PIN_MOVE_SENSOR));
 
   if (pirVal)
   {
-    FastLED.setBrightness(100);
+    myTimer = millis();
+    activeMode();
+  }
+  else if(millis() - myTimer >=10000){
+    waitingMode();     // counter меняется от 0 до 255 (тип данных byte)
+  }
+
+  // put your main code here, to run repeatedly:
+}
+
+void activeMode(){
+  FastLED.setBrightness(100 * bright);
     for (int i = 0; i < LED_COUNT; i++) {
     leds[i].setHue(counter + i * 255 / LED_COUNT);
     }
   counter++;        // counter меняется от 0 до 255 (тип данных byte)
   FastLED.show();
-    delay(3000);
-  }
-  else{
-    FastLED.setBrightness(30);
+}
+
+void waitingMode(){
+  FastLED.setBrightness(30 * bright);
     for (int i = 0; i < LED_COUNT; i++) {
     leds[i].setHue(counter + i * 255 / LED_COUNT);
-  }
-  FastLED.show();
-  counter++;        // counter меняется от 0 до 255 (тип данных byte)
-  }
-
-  // put your main code here, to run repeatedly:
+    }
+    FastLED.show();
+    counter++;
 }
