4c5a8ee9d29c9a682009650698d64a5179fd55f6
Suhanov <ivan.suhanov5@yandex.ru>
2022-02-11 19:32:13 +0300

    refactoring also

--- src/main.cpp
+++ src/main.cpp
@@ -9,6 +9,8 @@
 CRGB leds[LED_COUNT];
 byte counter;
 uint32_t myTimer;
+uint32_t myTimer1;
+uint32_t myTimer2;
 double bright = 1;
 
 void setup() {
@@ -33,6 +35,8 @@ int pirVal = digitalRead(PIN_MOVE_SENSOR);
   if (pirVal)
   {
     myTimer = millis();
+    myTimer1 = millis();
+    myTimer2 = millis();
     activeMode();
   }
   else if(millis() - myTimer >=10000){
@@ -44,18 +48,22 @@ int pirVal = digitalRead(PIN_MOVE_SENSOR);
 
 void activeMode(){
   FastLED.setBrightness(100 * bright);
+  if(millis() - myTimer1 >= 500){
     for (int i = 0; i < LED_COUNT; i++) {
     leds[i].setHue(counter + i * 255 / LED_COUNT);
     }
   counter++;        // counter меняется от 0 до 255 (тип данных byte)
   FastLED.show();
+  }
 }
 
 void waitingMode(){
   FastLED.setBrightness(30 * bright);
+  if(millis() - myTimer2 >= 500){
     for (int i = 0; i < LED_COUNT; i++) {
     leds[i].setHue(counter + i * 255 / LED_COUNT);
     }
     FastLED.show();
     counter++;
+  }
 }
